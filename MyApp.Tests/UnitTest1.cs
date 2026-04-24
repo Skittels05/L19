38881c89ec86259ba666811d36d7df0f09ab1a91
redouter <skittels261105@gmail.com>
2026-04-24 22:24:43 +0300

    add project

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using MyApp;
+namespace MyApp.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void TestAdd() {
+        var calc = new Calculator();
+        Assert.Equal(10, calc.Add(4, 6));
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            int result = calc.Add(5, 5);
+
+            Console.WriteLine($"5 + 5 = {result}");
+        }
+    }
+
+    public class Calculator 
+    {
+        public int Add(int a, int b) => a + b;
+    }
+}
